38a79357e8a1b6a86e9ddbad0607b6d6b71c1627
aing810 <aing810@aucklanduni.ac.nz>
2023-05-06 03:01:32 +1200

    POST room
    create a new room by a given code

---
+++ backend/src/routes/api/room.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { Room } from '../../db/schema/room';
+
+const router = express.Router();
+
+const createRoom= async (code) => {
+    const dbRoom= new Room(code);
+    await dbRoom.save();
+    return dbRoom;
+        
+  };
+
+router.post('/', async (req, res) => {
+
+    const { code } = req.body;
+
+    const room = await createDog(code);
+    return res.status(200).json(room)
+});
+
+export default router
