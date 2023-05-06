57b0f41c2a0b8782eaa811d02811a2c587909656
aing810 <aing810@aucklanduni.ac.nz>
2023-05-06 17:28:00 +1200

    commenting room functions

--- backend/src/db/endpointFunctions/room.js
+++ backend/src/db/endpointFunctions/room.js
@@ -1,11 +1,20 @@
 import { Room } from "../../db/schema/room";
 
+/**
+ * Creates a new Room by a given code
+ * @param {String} code Room code
+ * @returns the newly created room
+ */
 const createRoom = async (code) => {
   const dbRoom = new Room({ code });
   await dbRoom.save();
   return dbRoom;
 };
 
+/**
+ * Queries and retrieve every room in the database
+ * @returns list of all rooms
+ */
 const getAllRooms = async () => {
   return Room.find();
 };
