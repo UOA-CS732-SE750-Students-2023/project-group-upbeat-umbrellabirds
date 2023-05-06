608fff8746a270ba0a8f9121c059f3b3be2d9c99
aing810 <aing810@aucklanduni.ac.nz>
2023-05-07 04:49:22 +1200

    Styling for Lobby Screen
    centered button with profile above as the lobby owner, randomised location for other users

--- frontend/src/components/player-profile.jsx
+++ frontend/src/components/player-profile.jsx
@@ -3,13 +3,43 @@ import PropTypes from 'prop-types';
 import './player-profile.css'
 
 function PlayerProfile(props) {
+  const centerX = window.innerWidth / 2;
+  const centerY = window.innerHeight / 2;
+  const radius = 500; // distance from center to avoid
 
+  let x, y;
+  do {
+    x = Math.random() * (window.innerWidth - 200);
+    y = Math.random() * (window.innerHeight - 200);
+  } while (Math.sqrt((x - centerX) ** 2 + (y - centerY) ** 2) < radius);
+
+  if (props.random == "true" ){
+    const style = {
+      position: 'absolute',
+      top: y + 'px',
+      left: x + 'px',
+      margin: "10px"
+    };
     return (
-        <div className="player-profile">
+      <div className="player-profile" style={style}>
         <img className="player-picture" src={props.picture} alt="Player" />
         <h2 className='player-name'>{props.name}</h2>
-        </div>
+      </div>
     );
+  }else{
+    const style = {
+      position: 'absolute',
+    };
+    return (
+      <div className="player-profile" style={style}>
+        <img className="player-picture" src={props.picture} alt="Player" />
+        <h2 className='player-name'>{props.name}</h2>
+      </div>
+    );
+  }
+
+
+
 }
 
 PlayerProfile.propTypes = {
@@ -18,5 +48,3 @@ PlayerProfile.propTypes = {
 };
 
 export default PlayerProfile;
-
-
