e21f63d7e37e4a5bd59dd4f6f180f72dbad2ec8e
aing810 <aing810@aucklanduni.ac.nz>
2023-05-07 03:35:11 +1200

    Huggingface endpoint
    Sentence checker, downgraded axios for it to work

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -6,9 +6,11 @@ import roomRoutes from "./room";
 import playerRoutes from "./player";
 import gameRoutes from "./game";
 import openaiRoutes from "./openai";
+import sentenceRoutes from "./sentence";
 
 router.use("/room", roomRoutes);
 router.use("/player", playerRoutes);
 router.use("/game", gameRoutes);
 router.use("/openai", openaiRoutes);
+router.use("/sentence", sentenceRoutes);
 export default router;
