bb321ec7d70bceda5bae39a12746c40f099d7d7d
cyin631 <cyin631@aucklanduni.ac.nz>
2023-05-09 03:24:09 +1200

    Added image assets
    Co-Authored-By: MasonBango <101238495+MasonBango@users.noreply.github.com>
    Co-Authored-By: nicozhang123 <127396171+nicozhang123@users.noreply.github.com>
    Co-Authored-By: Zixuan Wang <31840342+zwan843@users.noreply.github.com>

--- frontend/src/Pages/GameResultsPage/index.jsx
+++ frontend/src/Pages/GameResultsPage/index.jsx
@@ -9,6 +9,9 @@ import usePost from "../../hooks/usePost";
 import useGet from "../../hooks/useGet";
 import PlayerProfile from "../../components/player-profile";
 import defaultLogo from "./../../assets/default-profile.jpg"
+import placeholder from "./../../assets/placeholder-img.png"
+import audioOn from "./../../assets/audio-on.png"
+import homeIcon from "./../../assets/home-icon.png"
 
 
 function GameResults() {
@@ -16,26 +19,26 @@ function GameResults() {
         <div className="App-ResultsPage">
             <div className="Results_header">
                 <h3>Final Score Screen</h3>
-                <img src="../../../public/images/微信图片_20230508102928_03.gif" alt="" style={{ right: "20px", }} />
-                <img src="../../../public/images/微信图片_20230508102928_05.gif" alt="" style={{ left: "20px", }} />
+                <img src={audioOn} alt="" style={{ right: "20px", }} />
+                <img src={homeIcon} alt="" style={{ left: "20px", }} />
             </div>
             <div className="Results_content">
                 <div className="Results_content_top">
                     <div className="Results_content_item">
                         <p>13 pts</p>
-                        <img src="../../../public/images/微信图片_20230508102928_16.gif" alt=""/>
+                        <img src={placeholder} alt=""/>
                         <p style={{  background: "#2095f2", borderTopLeftRadius: "20px" }}></p>
                         <p >2</p>
                     </div>
                     <div className="Results_content_item">
                         <p>19 pts</p>
-                        <img src="../../../public/images/微信图片_20230508102928_16.gif" alt=""/>
+                        <img src={placeholder} alt=""/>
                         <p style={{  background: "#fe3c00",  borderTopLeftRadius: "20px", borderTopRightRadius: "20px" }}></p>
                         <p>1</p>
                     </div>
                     <div className="Results_content_item">
                         <p>9 pts</p>
-                        <img src="../../../public/images/微信图片_20230508102928_16.gif" alt=""/>
+                        <img src={placeholder} alt=""/>
                         <p style={{  background: "#4baf4f",  borderTopRightRadius: "20px" }}></p>
                         <p>3</p>
                     </div>
@@ -44,19 +47,19 @@ function GameResults() {
                     <ul>
                         <li>
                             <span>#4</span>
-                            <img src="../../../public/images/微信图片_20230508102928_16.gif" alt="" />
+                            <img src={placeholder} alt="" />
                             <span>Name</span>
                             <span>8pts</span>
                         </li>
                         <li>
                             <span>#5</span>
-                            <img src="../../../public/images/微信图片_20230508102928_16.gif" alt="" />
+                            <img src={placeholder} alt="" />
                             <span>Name</span>
                             <span>7pts</span>
                         </li>
                         <li>
                             <span>#6</span>
-                            <img src="../../../public/images/微信图片_20230508102928_16.gif" alt="" />
+                            <img src={placeholder} alt="" />
                             <span>Name</span>
                             <span>5pts</span>
                         </li>
@@ -64,19 +67,19 @@ function GameResults() {
                     <ul>
                         <li>
                             <span>#7</span>
-                            <img src="../../../public/images/微信图片_20230508102928_16.gif" alt="" />
+                            <img src={placeholder} alt="" />
                             <span>Name</span>
                             <span>4pts</span>
                         </li>
                         <li>
                             <span>#8</span>
-                            <img src="../../../public/images/微信图片_20230508102928_16.gif" alt="" />
+                            <img src={placeholder} alt="" />
                             <span>Name</span>
                             <span>3pts</span>
                         </li>
                         <li>
                             <span>#9</span>
-                            <img src="../../../public/images/微信图片_20230508102928_16.gif" alt="" />
+                            <img src={placeholder} alt="" />
                             <span>Name</span>
                             <span>2pts</span>
                         </li>
@@ -84,7 +87,7 @@ function GameResults() {
                 </div>
             </div>
             <div className="Results_bottom">
-                <div>Play Again</div>
+                <CustomButton>Play Again</CustomButton>
             </div>
         </div>
     );
