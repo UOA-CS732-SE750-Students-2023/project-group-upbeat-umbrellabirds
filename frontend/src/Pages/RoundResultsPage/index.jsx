0c5ed5c056950e889a5203a54ffc0465e0dcc712
cyin631 <cyin631@aucklanduni.ac.nz>
2023-05-11 02:06:06 +1200

    Half fixed round results page

--- frontend/src/Pages/RoundResultsPage/index.jsx
+++ frontend/src/Pages/RoundResultsPage/index.jsx
@@ -2,27 +2,44 @@ import React from "react";
 import "./index.css";
 import PlayerProfile from "../../components/player-profile";
 import StartIcon from "../../assets/start-icon.png";
+import placeholder from "../../assets/placeholder-img.png";
+import firstMedal from "../../assets/1st.png";
+import secondMedal from "../../assets/2nd.png";
+import thirdMedal from "../../assets/3rd.png";
 
 function RoundResults() {
-  return (
-    <div className="page-container">
-      <div className="first-place">
-        <PlayerProfile picture={StartIcon} name={"aden"} random="false" />
-      </div>
-      <div className="page-header">
-        <h1>Round # Results</h1>
-      </div>
-      <div className="second-place">
-        <PlayerProfile picture={StartIcon} name={"aden"} random="false" />
-      </div>
-      <div className="third-place">
-        <PlayerProfile picture={StartIcon} name={"aden"} random="false" />
-      </div>
-      <div className="page-footer">
-        <h1>test</h1>
-      </div>
-    </div>
-  );
+
+    {/* <PlayerProfile picture={StartIcon} name={"aden"} random="false" /> */ }
+    return (
+        <div className="page-container">
+
+            <div className="page-header">
+                <h1>Round # Results</h1>
+            </div>
+
+            <div className="first-place">
+                <PlayerProfile picture={placeholder} name={"aden"} random="false" />
+                <p>Placeholder Text</p>
+                <img className="first-medal" src={firstMedal}/>
+            </div>
+
+            <div className="second-place">
+                <PlayerProfile picture={placeholder} name={"aden"} random="false" />
+                <p></p>
+                <img className="second-medal"src={secondMedal}/>
+            </div>
+
+            <div className="third-place">
+                <PlayerProfile picture={placeholder} name={"aden"} random="false" />
+                <p></p>
+                <img className="third-medal"src={thirdMedal}/>
+            </div>
+
+            <div className="page-footer">
+                <h1>test</h1>
+            </div>
+        </div>
+    );
 }
 
 export default RoundResults;
---
+++ frontend/src/Pages/RoundResultsPage/test.jsx
@@ -0,0 +1,42 @@
+<div className="ratings-container">
+
+<div className="First">
+    <div className="FirstImage">
+        {/* <PlayerProfile picture={StartIcon} name={"aden"} random="false" /> */}
+    </div>
+    <div className="FirstMedal">
+
+    </div>
+    <div claclassNamess="FirstGuess">
+
+    </div>
+</div>
+<div className="Second">
+    <div className="SecondImage">
+        {/* <PlayerProfile picture={StartIcon} name={"aden"} random="false" /> */}
+    </div>
+    <div class="SecondMedal">
+
+    </div>
+    <div class="SecondGuess">
+
+    </div>
+</div>
+<div className="Third">
+    <div className="ThirdImage">
+        {/* <PlayerProfile picture={StartIcon} name={"aden"} random="false" /> */}
+    </div>
+    <div className="ThirdMedal">
+
+    </div>
+    <div className="ThirdGuess">
+
+    </div>
+</div>
+<div className="Header">
+    <h1 className="heading">Round {round} Results</h1>
+</div>
+<div className="Footer">
+    {/* <h1>test</h1> */}
+</div>
+</div>
