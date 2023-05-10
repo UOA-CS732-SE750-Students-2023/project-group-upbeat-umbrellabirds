a1968ec71b685afa5caf762e0ac74c72808d07e3
aing810 <aing810@aucklanduni.ac.nz>
2023-05-10 21:21:39 +1200

    Made Music constant
    Co-Authored-By: MasonBango <101238495+MasonBango@users.noreply.github.com>
    Co-Authored-By: nicozhang123 <127396171+nicozhang123@users.noreply.github.com>
    Co-Authored-By: Zixuan Wang <31840342+zwan843@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,14 @@ import Ratings from "./Pages/RatingsPage";
 import GameResults from "./Pages/GameResultsPage";
 import RatingsPage from "./Pages/RatingsPage"
 import background_img from "./assets/react.svg";
+import MusicPlayer from "./components/MusicPlayer";
+import music from "./assets/music.mp3"
 import "./App.css";
 
 const App = () => {
   return (
       <div className="container">
+      <MusicPlayer audioUrl={music} volume={0.2} isMuted={false}></MusicPlayer>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lobby" element={<Lobby />} />
