47ce934fdd0c2036383cf537fb88da55c562fd4b
cyin631 <cyin631@aucklanduni.ac.nz>
2023-05-11 18:23:38 +1200

    Update correct App.jsx routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,26 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/HomePage";
 import Lobby from "./Pages/LobbyPage";
-import GamePage from "./Pages/GamePage";
-import TestPage from "./Pages/TestPage";
-import RatingsPage from "./Pages/RatingsPage";
-import ResultsPage from "./Pages/GameResultsPage";
+import RoundResults from "./Pages/RoundResultsPage";
+import Ratings from "./Pages/RatingsPage";
+import GameResults from "./Pages/GameResultsPage";
+import RatingsPage from "./Pages/RatingsPage"
 import background_img from "./assets/react.svg";
+import MusicPlayer from "./components/MusicPlayer";
+import music from "./assets/music.mp3"
 import "./App.css";
 
 const App = () => {
   return (
       <div className="container">
+      <MusicPlayer audioUrl={music} volume={0.2} isMuted={false}></MusicPlayer>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lobby" element={<Lobby />} />
-          <Route path="/game" element={<GamePage />} />
-          <Route path="/test" element={<TestPage />} />
-          {/* <Route path="/ratingsPage" element={<RatingsPage />} />
-          <Route path="/resultsPage" element={<ResultsPage />} /> */}
+          <Route path="/roundResults" element={<RoundResults />} />
+          <Route path="/ratings" element={<Ratings />} />
+          <Route path="/gameResults" element={<GameResults />} />
+          <Route path="/ratingsPage" element={<RatingsPage />} />
         </Routes>
       </div>
   );
