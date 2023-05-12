a8c6b921a4d82baee89a399bf2c7e1ec48569eee
Shirley Shao <shirley.shao@datacom.co.nz>
2023-05-12 22:36:23 +1200

    usePost update

--- frontend/test/usePost.test.jsx
+++ frontend/test/usePost.test.jsx
@@ -1,7 +1,32 @@
+import axios from 'axios';
 import usePost from '../src/hooks/usePost';
 
+jest.mock('axios');
+
 describe('usePost', () => {
-  it('should be defined', () => {
-    expect(usePost).toBeDefined();
+  it('should post data successfully', async () => {
+    const url = '/api/data';
+    const body = { id: 1 };
+    const responseData = { data: 'Post success' };
+
+    axios.post.mockResolvedValueOnce({ data: responseData });
+
+    const result = await usePost(url, body);
+
+    expect(axios.post).toHaveBeenCalledWith(url, body);
+    expect(result).toEqual(responseData);
+  });
+
+  it('should handle error', async () => {
+    const url = '/api/data';
+    const body = { id: 1 };
+    const errorResponse = { response: { status: 404, data: 'Not found' } };
+
+    axios.post.mockRejectedValueOnce(errorResponse);
+
+    const result = await usePost(url, body);
+
+    expect(axios.post).toHaveBeenCalledWith(url, body);
+    expect(result).toEqual('Not found');
   });
 });
