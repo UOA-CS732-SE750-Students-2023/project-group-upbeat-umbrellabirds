0458558660efe82bf4d1bcd12f9c8fe72f605a09
Shirley Shao <shirley.shao@datacom.co.nz>
2023-05-12 22:39:06 +1200

    update hook scripts

--- frontend/test/usePut.test.jsx
+++ frontend/test/usePut.test.jsx
@@ -1,7 +1,32 @@
+import axios from 'axios';
 import usePut from '../src/hooks/usePut';
 
+jest.mock('axios');
+
 describe('usePut', () => {
-  it('should be defined', () => {
-    expect(usePut).toBeDefined();
+  it('should put data successfully', async () => {
+    const url = '/api/data';
+    const body = { id: 1 };
+    const responseData = { data: 'Put success' };
+
+    axios.put.mockResolvedValueOnce({ data: responseData });
+
+    const result = await usePut(url, body);
+
+    expect(axios.put).toHaveBeenCalledWith(url, body);
+    expect(result).toEqual(responseData);
+  });
+
+  it('should handle error', async () => {
+    const url = '/api/data';
+    const body = { id: 1 };
+    const errorResponse = { response: { status: 404, data: 'Not found' } };
+
+    axios.put.mockRejectedValueOnce(errorResponse);
+
+    const result = await usePut(url, body);
+
+    expect(axios.put).toHaveBeenCalledWith(url, body);
+    expect(result).toEqual('Not found');
   });
 });
