c96ef787fb8da45659f83338284c5e7a7c1a91ce
aing810 <aing810@aucklanduni.ac.nz>
2023-05-26 16:08:51 +1200

    Revert "reverted changes"
    This reverts commit 24149c9271274bca1b475954cdc8fc41ed83a8f9.

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,106 @@ const io = new Server(server, {
   },
 });
 
+// Setup middleware
+io.origin("*");
+
+io.listen("4000");
+
+
+
+let guessedPlayers = [];
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.on("joinRoom", ({ roomName, playerName }) => {
+    console.log("Joining room " + roomName);
+    socket.join(roomName);
+    io.in(roomName).emit("playerJoined", playerName);
+    console.log(io.sockets.adapter.rooms);
+  });
+
+  socket.on("disconnect", ({roomCode}) => {
+    console.log("Client disconnected: " + roomCode);
+  });
+
+  socket.on("removePlayer", ({roomCode, playerID}) => {
+    console.log("Removing player " + playerID + " from room " + roomCode);
+    io.in(roomCode).emit("playerRemoved", playerID);
+    socket.leave(roomCode);
+  });
+
+  socket.on("startGame", ({roomCode})=> {
+    console.log("Game started");
+    io.in(roomCode).emit("gameStarted", roomCode);
+  });
+
+  socket.on("newMessage", ({message, roomCode}) => {
+    console.log("New message: " + message);
+    io.in(roomCode).emit("getMessage", message);
+  });
+
+  socket.on("gameInfoChange", ({gameInfo, roomInfo}) => {
+    console.log("Game info changed");
+    // console.log(gameInfo)
+    io.in(roomInfo).emit("setGameInfo", gameInfo);
+  });
+
+  socket.on("getRooms", ({roomInfo}) => {
+    console.log("Getting rooms");
+    io.in(roomInfo).emit("rooms", io.sockets.adapter.rooms);
+    console.log(io.sockets.adapter.rooms)
+  });
+
+  socket.on("tester", ({tester, roomInfo}) => {
+    console.log("tester", + tester)
+    io.in(roomInfo).emit("tester");
+    
+  });
+  socket.on("timerReset", ({roomInfo}) => {
+    console.log("timerReset")
+    io.in(roomInfo).emit("timerReset");
+  });
+  
+  socket.on("roundResults", ({firstPlayer, secondPlayer, thirdPlayer, roomInfo}) => {
+    console.log("roundResults")
+    io.in(roomInfo).emit("getRoundResults", firstPlayer, secondPlayer, thirdPlayer);
+  })
+  socket.on("roundDone", ({roomInfo}) => {
+    console.log("roundDone")
+    io.in(roomInfo).emit("roundDone");
+  })
+
+  socket.on("guessed", ({playerId, roomInfo,curGuess}) => {
+    console.log("guessed", playerId);
+    let playerID = playerId;
+    console.log(io.sockets.adapter.rooms.get(roomInfo).size);
+    guessedPlayers.push({playerID, curGuess});
+    console.log(guessedPlayers)
+    if(guessedPlayers.length == io.sockets.adapter.rooms.get(roomInfo).size){
+      console.log("allGuessed")
+      io.in(roomInfo).emit("allGuessed", guessedPlayers);
+      guessedPlayers = [];
+    }
+  })
+
+  socket.on("SendingRoundResults", ({roomInfo}) => {
+    console.log("SendingRoundResults");
+    io.in(roomInfo).emit("recievingRoundResults");
+  })
+
+  socket.on("setRoundNumber", ({roomInfo, roundNum}) => {
+    console.log("setRoundNumber");
+    io.in(roomInfo).emit("getRoundNumber", roundNum);
+  }
+  )
+
+  socket.on("startCountdown", ({roomInfo}) => {
+    console.log("startCountdown");
+    io.in(roomInfo).emit("startCountdown");
+  })
+
+
+});
 
 // Setup body-parser
 
@@ -37,111 +137,8 @@ app.get("/", (req, res) => {
   res.json("Hello, world!");
 });
 
-
-// Setup middleware
-io.listen("4000");
-
-let guessedPlayers = [];
-
-io.on("connection", (socket) => {
-  console.log("Client connected");
-  socket.on("joinRoom", ({ roomName, playerName }) => {
-    console.log("Joining room " + roomName);
-    socket.join(roomName);
-    io.in(roomName).emit("playerJoined", playerName);
-    console.log(io.sockets.adapter.rooms);
-  });
-
-  socket.on("disconnect", ({ roomCode }) => {
-    console.log("Client disconnected: " + roomCode);
-  });
-
-  socket.on("removePlayer", ({ roomCode, playerID }) => {
-    console.log("Removing player " + playerID + " from room " + roomCode);
-    io.in(roomCode).emit("playerRemoved", playerID);
-    socket.leave(roomCode);
-  });
-
-  socket.on("startGame", ({ roomCode }) => {
-    console.log("Game started");
-    io.in(roomCode).emit("gameStarted", roomCode);
-  });
-
-  socket.on("newMessage", ({ message, roomCode }) => {
-    console.log("New message: " + message);
-    io.in(roomCode).emit("getMessage", message);
-  });
-
-  socket.on("gameInfoChange", ({ gameInfo, roomInfo }) => {
-    console.log("Game info changed");
-    // console.log(gameInfo)
-    io.in(roomInfo).emit("setGameInfo", gameInfo);
-  });
-
-  socket.on("getRooms", ({ roomInfo }) => {
-    console.log("Getting rooms");
-    io.in(roomInfo).emit("rooms", io.sockets.adapter.rooms);
-    console.log(io.sockets.adapter.rooms);
-  });
-
-  socket.on("tester", ({ tester, roomInfo }) => {
-    console.log("tester", +tester);
-    io.in(roomInfo).emit("tester");
-  });
-  socket.on("timerReset", ({ roomInfo }) => {
-    console.log("timerReset");
-    io.in(roomInfo).emit("timerReset");
-  });
-
-  socket.on(
-    "roundResults",
-    ({ firstPlayer, secondPlayer, thirdPlayer, roomInfo }) => {
-      console.log("roundResults");
-      io.in(roomInfo).emit(
-        "getRoundResults",
-        firstPlayer,
-        secondPlayer,
-        thirdPlayer
-      );
-    }
-  );
-  socket.on("roundDone", ({ roomInfo }) => {
-    console.log("roundDone");
-    io.in(roomInfo).emit("roundDone");
-  });
-
-  socket.on("guessed", ({ playerId, roomInfo, curGuess }) => {
-    console.log("guessed", playerId);
-    let playerID = playerId;
-    console.log(io.sockets.adapter.rooms.get(roomInfo).size);
-    guessedPlayers.push({ playerID, curGuess });
-    console.log(guessedPlayers);
-    if (guessedPlayers.length == io.sockets.adapter.rooms.get(roomInfo).size) {
-      console.log("allGuessed");
-      io.in(roomInfo).emit("allGuessed", guessedPlayers);
-      guessedPlayers = [];
-    }
-  });
-
-  socket.on("SendingRoundResults", ({ roomInfo }) => {
-    console.log("SendingRoundResults");
-    io.in(roomInfo).emit("recievingRoundResults");
-  });
-
-  socket.on("setRoundNumber", ({ roomInfo, roundNum }) => {
-    console.log("setRoundNumber");
-    io.in(roomInfo).emit("getRoundNumber", roundNum);
-  });
-
-  socket.on("startCountdown", ({ roomInfo }) => {
-    console.log("startCountdown");
-    io.in(roomInfo).emit("startCountdown");
-  });
-});
 // Start the DB running. Then, once it's connected, start the server.
 connectToDatabase().then(function () {
   // eslint-disable-next-line no-console
   app.listen(port, () => console.log(`App server listening on port ${port}! `));
-
-  
 });
